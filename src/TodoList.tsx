bdddc2b5d0d55acb0a7d95357d7b12b049e2bf74
Temuulen <temhuuz@gmail.com>
2023-09-26 20:36:40 +0800

    added edit and delete

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,44 +1,103 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 
-interface item {
-    id: number;
-    text: string;
-    completed: boolean;
-
+interface Item {
+  id: number;
+  text: string;
+  completed: boolean;
 }
 
-export const TodoList: React.FC =() => {
-    const [todos, setTodos] =useState<item[]>([
-        {id: 1, text: "Make todo list", completed: false},
-        {id: 2, text: "Deploy to vercel", completed: false}
-    ]);
-    const [input , setInput] = useState<string>("");
-    const handleToggle = (id: number) => {
-        setTodos(
-            todos.map((todo)=>{
-                if(todo.id === id){
-                    return {...todo, completed: !todo.completed};
-                }
-                return todo;
-            })
-        )
-    }
-    const handleClick = () => {
-        const newTodo: item = {id: Date.now(), text: input, completed: false};
-        setTodos([...todos, newTodo]);
-    }
-    return <div className="main-container">
-        <h1>Todo List</h1>
-        <ul>
-           {todos.map((todo)=> (
-            <li key={todo.id} onClick={() => handleToggle(todo.id)} style={{textDecoration: todo.completed ? "line-through" : "none"}}>
-                {todo.text}
-            </li>
-           ))}
-        </ul>
-        <input type="text" placeholder="Add todo item" onChange={(e)=> setInput(e.currentTarget.value)}/>
-        <button onClick={handleClick}>
-            Add
-        </button>
+export const TodoList: React.FC = () => {
+  const [todos, setTodos] = useState<Item[]>([
+    { id: 1, text: "Make todo list", completed: false },
+    { id: 2, text: "Deploy to Vercel", completed: false },
+  ]);
+  const [input, setInput] = useState<string>("");
+  const [editingTodoId, setEditingTodoId] = useState<number | null>(null);
+  const [editedText, setEditedText] = useState<string>("");
+
+  const handleToggle = (id: number) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        }
+        return todo;
+      })
+    );
+  };
+
+  const handleClick = () => {
+    const newTodo: Item = { id: Date.now(), text: input, completed: false };
+    setTodos([...todos, newTodo]);
+    setInput("");
+  };
+
+  const handleDelete = (id: number) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const handleEditStart = (id: number, text: string) => {
+    setEditingTodoId(id);
+    setEditedText(text);
+  };
+
+  const handleEditCancel = () => {
+    setEditingTodoId(null);
+    setEditedText("");
+  };
+
+  const handleEditSave = (id: number) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, text: editedText };
+        }
+        return todo;
+      })
+    );
+    setEditingTodoId(null);
+  };
+
+  return (
+    <div className="main-container">
+      <h1>Todo List</h1>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id}>
+            {editingTodoId === todo.id ? (
+              <>
+                <input
+                  type="text"
+                  value={editedText}
+                  onChange={(e) => setEditedText(e.target.value)}
+                />
+                <button onClick={() => handleEditSave(todo.id)}>Save</button>
+                <button onClick={handleEditCancel}>Cancel</button>
+              </>
+            ) : (
+              <>
+                <span
+                  onClick={() => handleToggle(todo.id)}
+                  style={{
+                    textDecoration: todo.completed ? "line-through" : "none",
+                  }}
+                >
+                  {todo.text}
+                </span>
+                <button onClick={() => handleEditStart(todo.id, todo.text)}>Edit</button>
+                <button onClick={() => handleDelete(todo.id)}>Delete</button>
+              </>
+            )}
+          </li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        placeholder="Add todo item"
+        value={input}
+        onChange={(e) => setInput(e.currentTarget.value)}
+      />
+      <button onClick={handleClick}>Add</button>
     </div>
-}
+  );
+};
